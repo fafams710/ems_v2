be031df05de2a0a0a0b2c79ded7a7e4e74974e8e
Franz_Puno <Franz@gmail.com>
2024-11-02 23:12:22 +0800

    new settings database

--- emsssss/settings.py
+++ emsssss/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-&aa87%$k!_koae4=5cp4^t_0t$-&%61g)nl5(j@@3v$_3%7c3u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['emsv4-d33ccf5eba5b.herokuapp.com']
+ALLOWED_HOSTS = ['emsv4-d33ccf5eba5b.herokuapp.com',
+                 '127.0.0.1']
 
 
 # Application definition
@@ -81,7 +82,7 @@ WSGI_APPLICATION = 'emsssss.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / "db.sqlite3"),
     }
 }
 
