de3acd2d26527d6853313cc5be40f4e5378ec4d3
fafams710 <krparbo@student.hau.edu.ph>
2024-11-03 10:29:19 +0800

    try lang

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import AuthCode
 from .models import LeaveRequest, EmployeeProfile
+
 class AuthCodeAdmin(admin.ModelAdmin):
     list_display = ['user', 'code', 'is_active', 'created_at']  # Correctly reference the fields
     list_filter = ['is_active', 'created_at']  # These fields now exist in the model
--- emsssss/settings.py
+++ emsssss/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import os
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,7 +80,9 @@ WSGI_APPLICATION = 'emsssss.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        engine='django.db.backends.postgresql')
 }
 
 
